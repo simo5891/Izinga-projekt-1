c01bc9d2b232e25be03f608696edc90cb9a4b76b
Christian Simonsen <chri07w0@edu.eal.dk>
2017-10-03 12:59:53 +0200

    Added Model.LogFileItem

---
+++ Izinga projekt/Izinga projekt/Models/LogFileItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IzingaProjekt.Models
+{
+    public class LogFileItem
+    {
+        public string Time { get; set; }
+        public int ID { get; set; }
+        public string Alarm { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Accommodation { get; set; }
+        public bool Unsubscribed { get; set; }
+
+        public LogFileItem()
+        { }
+    }
+}
